7cb64f4f59b874eb4997d7c855995516b5e2207b
Chevy Ray Johnston <happytrash@gmail.com>
2026-04-24 11:32:41 -0700

    guid box method defs

--- crates/fey_guid/lua/Guid.lua
+++ crates/fey_guid/lua/Guid.lua
@@ -15,4 +15,22 @@ local methods = {}
 ---@nodiscard
 function module.new() end
 
+---Returns a temporary copy of this value.
+---@param self Guid
+---@return Guid
+---@nodiscard
+function methods.clone(self) end
+
+---Boxes the value if not already boxed.
+---@param self Guid
+---@return Guid
+---@nodiscard
+function methods.box(self) end
+
+---Boxes a copy of this value.
+---@param self Guid
+---@return Guid
+---@nodiscard
+function methods.box_clone(self) end
+
 return module
